fa694a274df5b7b62dc50d024e8fb5c8d4e921f1
APTYPMORGAN <arturkuriev5@gmail.com>
2026-09-13 23:24:08 +0300

    learn dart cycles

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -64,6 +64,7 @@ void main() {
   for (var fruit in fruits2) {
     print(fruit);
   }
+
   print(greet('Артур'));
   print(greet('Артём'));
   print(square(5));
@@ -80,4 +81,43 @@ void main() {
   print(upper);
   List<String> longNames = names.where ((name) => name.length > 4).toList();
   print(longNames);
+  int score2 = 85;
+  String grade;
+  if (score2 >= 90){
+     grade = 'A';
+  } else if (score2 >= 75) {
+    grade = 'B';
+  } else {
+    grade = 'C';
+  }
+  print(grade);
+  String result = score >= 60 ? 'Сдал' : 'Нe сдал';
+  print(result);
+  for (int i = 0; i < 5; i++){
+    print(i);
+  }
+  List<String> fruits3 = ['яблоко', 'банан', 'груша'];
+  for (var fruit2 in fruits3){
+    print(fruit2);
+  }
+  int n = 0;
+  while (n < 3){
+    print(n);
+    n++;
+  }
+  String day = 'Чт';
+  switch (day) {
+    case 'Сб':
+    case 'Вс':
+      print('Выходной');
+      break;
+    case 'Пн':
+      print('Начало недели');
+      break;
+    case 'Пт':
+      print('Конец рабочей недели');
+      break;
+    default:
+      print('Рабочий день');
+  }
 }
