10be6991b1a66145e8b1a0375346d01e98b7b039
APTYPMORGAN <arturkuriev5@gmail.com>
2026-09-13 23:46:57 +0300

    Add grade analyzer app

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,123 +1,180 @@
-String greet(String name) {
-  return 'Привет, $name!';
+double average(List<int> grades){
+    if (grades.isEmpty) return 0;
+    int sum = 0;
+    for (var grade in grades) {
+        sum += grade;
+    }
+    return sum / grades.length;
 }
-
-int square(int x) => x * x;
-String greet2(String name2) => 'Привет, $name2!';
-void describePet({
-  required String name,
-  String species = 'кот',
-  int age = 0,
-}) {
-  print('$name - $species, возраст $age');
+int MaxGrade(List<int> grades) {
+    int max = grades[0];
+    for (var grade in grades) {
+        if (grade > max) max = grade;
+    }
+    return max;
 }
-String repeat(String text, [int times = 2]) {
-  String result = '';
-  for (int i = 0; i < times; i++) {
-    result += text;
-  }
-  return result;
+int MinGrade(List<int> grades) {
+    int min = grades[0];
+    for (var grade in grades) {
+        if (grade < min) min = grade;
+    }
+    return min;
 }
-
+String LetterGrade(double avg){
+    if (avg >= 4.5) return 'Отлично';
+    if (avg >= 3.5) return 'Хорошо';
+    if (avg >= 2.5) return 'Удовлетворительно';
+    return 'Неудовлетворительно';
+}
+void printStats({required String name, required List<int> grades}) {
+    double avg = average(grades);
+    print(' $name');
+    print('Оценки: $grades');
+    print('Среднее: ${avg.toStringAsFixed(2)}');
+    print('Макс: ${MaxGrade(grades)}, Мин: ${MinGrade(grades)}');
+    print('Итог: ${LetterGrade(avg)}');
+    print('');
+}
 void main() {
-  String name = 'Артур';
-  int age = 17;
-  double height = 1.78;
-  bool isStudent = true;
+    Map<String, List<int>> students = {
+    'Артём Иванов': [5, 4, 5, 3, 4, 5],
+    'Мария Петрова': [4, 4, 5, 5, 4, 5],
+    'Иван Сидоров': [3, 3, 4, 2, 3, 4],
+    };
 
-  print(name);
-  print(age);
-  print(height);
-  print(isStudent);
-
-  print('Привет, $name! Тебе $age лет.');
-  print('Через 5 лет тебе будет ${age + 5} лет.');
-  print('Рост: ${height} м, студент: $isStudent');
-
-  var score = 95;
-  var language = 'Dart';
-  print('$language: $score');
-
-  final int releaseYear = 2011;
-  const String appName = 'Lab1';
-  final int startYear = 2026;
-  print('$appName started in $startYear');
-
-  String? nickname = null;
-  String display = nickname ?? 'Аноним';
-  print(display);
-
-  List<String> fruits = ['яблоко', 'банан', 'груша'];
-  fruits.add('апельсин');
-  print(fruits[0]);
-  print(fruits.length);
-
-  Map<String, dynamic> person = {'name': 'Артур', 'age': '17'};
-  print(person['name']);
-  person['city'] = 'Волжский';
-
-  Set<int> ids = {1, 2, 3, 2, 1};
-  print(ids);
-  print(ids.length);
-
-  List<String> fruits2 = ['яблоко', 'банан', 'груша'];
-  for (var fruit in fruits2) {
-    print(fruit);
-  }
-
-  print(greet('Артур'));
-  print(greet('Артём'));
-  print(square(5));
-  print(greet2('Гриша'));
-  describePet(name: 'Барсик', age: 3);
-  describePet(name: 'Шарик', species: 'пёс');
-  print(repeat('ха', 3));
-  print(repeat('ха'));
-  List<int> numbers = [3, 1, 4, 1, 5, 9];
-  numbers.sort((a, b) => b - a);
-  print(numbers);
-  List<String> names = ['Артём', 'Мария', 'Иван'];
-  List<String> upper = names.map((name) => name.toUpperCase()).toList();
-  print(upper);
-  List<String> longNames = names.where ((name) => name.length > 4).toList();
-  print(longNames);
-  int score2 = 85;
-  String grade;
-  if (score2 >= 90){
-     grade = 'A';
-  } else if (score2 >= 75) {
-    grade = 'B';
-  } else {
-    grade = 'C';
-  }
-  print(grade);
-  String result = score >= 60 ? 'Сдал' : 'Нe сдал';
-  print(result);
-  for (int i = 0; i < 5; i++){
-    print(i);
-  }
-  List<String> fruits3 = ['яблоко', 'банан', 'груша'];
-  for (var fruit2 in fruits3){
-    print(fruit2);
-  }
-  int n = 0;
-  while (n < 3){
-    print(n);
-    n++;
-  }
-  String day = 'Чт';
-  switch (day) {
-    case 'Сб':
-    case 'Вс':
-      print('Выходной');
-      break;
-    case 'Пн':
-      print('Начало недели');
-      break;
-    case 'Пт':
-      print('Конец рабочей недели');
-      break;
-    default:
-      print('Рабочий день');
-  }
+    print('Анализатор оценок');
+    students.forEach((name, grades) {
+        printStats(name: name, grades: grades);
+    });
+    print('Общая статистика');
+    int totalStudents = students.length;
+    print('Всего студентов: $totalStudents');
+    int excellentCount = 0;
+    students.forEach((name, grades) {
+        if (average(grades) >= 4.5) excellentCount++;
+    });
+    print('Отличников: $excellentCount из $totalStudents');
 }
+// String greet(String name) {
+//   return 'Привет, $name!';
+// }
+
+// int square(int x) => x * x;
+// String greet2(String name2) => 'Привет, $name2!';
+// void describePet({
+//   required String name,
+//   String species = 'кот',
+//   int age = 0,
+// }) {
+//   print('$name - $species, возраст $age');
+// }
+// String repeat(String text, [int times = 2]) {
+//   String result = '';
+//   for (int i = 0; i < times; i++) {
+//     result += text;
+//   }
+//   return result;
+// }
+
+// void main() {
+//   String name = 'Артур';
+//   int age = 17;
+//   double height = 1.78;
+//   bool isStudent = true;
+
+//   print(name);
+//   print(age);
+//   print(height);
+//   print(isStudent);
+
+//   print('Привет, $name! Тебе $age лет.');
+//   print('Через 5 лет тебе будет ${age + 5} лет.');
+//   print('Рост: ${height} м, студент: $isStudent');
+
+//   var score = 95;
+//   var language = 'Dart';
+//   print('$language: $score');
+
+//   final int releaseYear = 2011;
+//   const String appName = 'Lab1';
+//   final int startYear = 2026;
+//   print('$appName started in $startYear');
+
+//   String? nickname = null;
+//   String display = nickname ?? 'Аноним';
+//   print(display);
+
+//   List<String> fruits = ['яблоко', 'банан', 'груша'];
+//   fruits.add('апельсин');
+//   print(fruits[0]);
+//   print(fruits.length);
+
+//   Map<String, dynamic> person = {'name': 'Артур', 'age': '17'};
+//   print(person['name']);
+//   person['city'] = 'Волжский';
+
+//   Set<int> ids = {1, 2, 3, 2, 1};
+//   print(ids);
+//   print(ids.length);
+
+//   List<String> fruits2 = ['яблоко', 'банан', 'груша'];
+//   for (var fruit in fruits2) {
+//     print(fruit);
+//   }
+
+//   print(greet('Артур'));
+//   print(greet('Артём'));
+//   print(square(5));
+//   print(greet2('Гриша'));
+//   describePet(name: 'Барсик', age: 3);
+//   describePet(name: 'Шарик', species: 'пёс');
+//   print(repeat('ха', 3));
+//   print(repeat('ха'));
+//   List<int> numbers = [3, 1, 4, 1, 5, 9];
+//   numbers.sort((a, b) => b - a);
+//   print(numbers);
+//   List<String> names = ['Артём', 'Мария', 'Иван'];
+//   List<String> upper = names.map((name) => name.toUpperCase()).toList();
+//   print(upper);
+//   List<String> longNames = names.where ((name) => name.length > 4).toList();
+//   print(longNames);
+//   int score2 = 85;
+//   String grade;
+//   if (score2 >= 90){
+//      grade = 'A';
+//   } else if (score2 >= 75) {
+//     grade = 'B';
+//   } else {
+//     grade = 'C';
+//   }
+//   print(grade);
+//   String result = score >= 60 ? 'Сдал' : 'Нe сдал';
+//   print(result);
+//   for (int i = 0; i < 5; i++){
+//     print(i);
+//   }
+//   List<String> fruits3 = ['яблоко', 'банан', 'груша'];
+//   for (var fruit2 in fruits3){
+//     print(fruit2);
+//   }
+//   int n = 0;
+//   while (n < 3){
+//     print(n);
+//     n++;
+//   }
+//   String day = 'Чт';
+//   switch (day) {
+//     case 'Сб':
+//     case 'Вс':
+//       print('Выходной');
+//       break;
+//     case 'Пн':
+//       print('Начало недели');
+//       break;
+//     case 'Пт':
+//       print('Конец рабочей недели');
+//       break;
+//     default:
+//       print('Рабочий день');
+//   }
+// }
